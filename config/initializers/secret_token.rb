3ae00d3cdfb8d3336fa92984345f04c27e230839
timestep <jbosshellokitty@gmail.com>
2013-08-20 14:07:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PolyApp::Application.config.secret_key_base = '1d7c95de550188a68cbcf48a301bce780eecda8c49dd7b680ae70f970897cfb1fd370f567455cda08af0bbb9f98067494ad41c47559718968364ed724d4112ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PolyApp::Application.config.session_store :cookie_store, key: '_poly-app_session'
