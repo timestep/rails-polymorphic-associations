94678c46705082718c77458c65a105b792c6cabd
timestep <jbosshellokitty@gmail.com>
2013-08-20 14:17:30 -0400

    generation of scafoold and models | created polymorphic association

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PolyApp::Application.routes.draw do
+  resources :comments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
